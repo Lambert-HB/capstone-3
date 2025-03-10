49788665f32c70a65622816faafc79bbfec6ebe7
Lambert-HB <BadenhLamb@outlook.com>
2025-03-10 13:57:44 +0200

    Accounts app Docstrings

--- accounts/views.py
+++ accounts/views.py
@@ -8,6 +8,17 @@ from django.views.generic import CreateView
 
 # Django generic Sign-up view
 class SignUpView(CreateView):
+    """
+    A Generic view to handle user registration.
+    
+    This view allows a user to create a new profile using the UserCreationForm.
+    If successful the user is redirected to the login page.
+
+    Args:
+        form_class (class): Form used for user signup.
+        success_url (str): Redirect url if successful.
+        template_name (str)= Path to template to render.
+    """
 
     form_class = UserCreationForm
     # Reverse to login after account creation
